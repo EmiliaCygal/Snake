4ec1f92573ee5444e2b3bcae20c6a91660248bc9
Emilia <ecygal99@gmail.com>
2023-04-04 19:36:42 +0200

    innitial commit

---
+++ src/main/java/SnakeGame.java
@@ -0,0 +1,12 @@
+public class  SnakeGame {
+
+    public static void main(String [] args) {
+
+        //GameFrame gameFrame = new GameFrame();
+        // if never planning to use the instance name 'gameFrame' can instead do a shorter version:
+
+        new GameFrame();
+
+    }
+
+}
